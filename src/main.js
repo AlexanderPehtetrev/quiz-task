5af20dcf7a11de3a7ed0b8a3016c9883854d5c78
Ршщршгкрпшр <aleksanderpehterev2611@gmail.com>
2024-09-28 16:33:29 +0300

    feat: button activity

---
+++ src/main.js
@@ -0,0 +1,95 @@
+const startWrapper = document.querySelector(".start-wrapper");
+const quizWrapper = document.querySelector(".quiz-wrapper");
+const startButton = document.querySelector(".start-button");
+const questionHeading = document.querySelector(".question-heading");
+const resultHeading = document.querySelector(".result-heading");
+const buttons = document.querySelectorAll(".answer-row button");
+
+let overallCounter = 0;
+let correctCounter = 0;
+let wrongCounter = 0;
+
+function countAnswers(answer, button) {
+    return function () {
+     overallCounter += 1;  
+     
+     if (String(answer) === button.innerText) {
+        correctCounter += 1;
+     } else {
+        wrongCounter += 1;
+     }
+    }
+}
+
+function showResults () {
+    quizWrapper.classList.add("hide");
+    startWrapper.classList.remove("hide");
+    resultHeading.classList.remove("hide");
+    resultHeading.innerText = `overall answers: ${overallCounter}, correct: ${correctCounter}, wrong: ${wrongCounter}, `;
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function generateCorrectButton(answer) {
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", countAnswers(answer, button));
+    })
+
+    buttons.forEach ((button) => {
+        button.addEventListener("click", generateQuastion);
+
+        let value = getRandomInt(100);
+        while (value === answer) {
+            value = getRandomInt(100);
+        }
+        button.innerText = value;
+
+     button.classList.remove("correct-button");
+     button.classList.add("wrong-button");
+    })
+
+    const index = getRandomInt(buttons.length);
+    const correctButton = buttons[index];
+    correctButton.innerText = answer;
+    correctButton.classList.remove("wrong-button");
+    correctButton.classList.add("correct-button");
+}
+
+function startQuiz() {
+startWrapper.classList.add("hide");
+quizWrapper.classList.remove("hide");
+generateQuastion();
+setTimeout(showResults, 10000);
+}
+
+
+function generateSign() {
+    if (getRandomInt(2) == 0) {
+        return "-"
+    }
+    else {
+        return "+"
+    }
+}
+
+function generateQuastion() {
+    const number1 = getRandomInt(50);
+    const number2 = getRandomInt(50);
+    const sign = generateSign();
+    let answer;
+
+    if (sign === "+") {
+         answer = number1 + number2
+    }
+    else {
+        answer = number1 - number2
+    }
+
+    questionHeading.innerText = `${number1} ${sign} ${number2}`;
+    generateCorrectButton(answer)
+}
+
+startButton.addEventListener("click", startQuiz);
